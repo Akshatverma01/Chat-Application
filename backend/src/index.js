675df9c6a15f84ae92268f3ab12c2c302bb09b23
Akshatverma01 <akshat0104verma@gmail.com>
2025-04-25 00:28:29 +0530

    feat: initialize project structure with backend and frontend configurations, add environment handling, and update dependencies completed project

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+
+import path from "path";
+
 import {connectDB} from "./lib/db.js"
 
 import authRoutes from "./routes/auth.route.js";
@@ -21,9 +24,19 @@ app.use(cors({
     credentials:true
 }))
 const PORT = process.env.PORT;
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV ==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{m 
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 connectDB()
 .then(()=>{
